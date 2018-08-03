4ffeb230e5154cbdc2dac51d54812625bd167ea8
prasanjitdey <pdey@iaai.com>
2018-08-03 19:29:06 +0530

    animate added with fade in

--- app/src/main/java/com/prasanjit/smoothloading/MainActivity.java
+++ app/src/main/java/com/prasanjit/smoothloading/MainActivity.java
@@ -2,6 +2,10 @@ package com.prasanjit.smoothloading;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.prasanjit.smoothloadinglib.SmoothLoadingView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SmoothLoadingView btnLoad = findViewById(R.id.btn_load);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Smooth loading view", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- smoothloadinglib/src/main/java/com/prasanjit/smoothloadinglib/SmoothLoadingView.java
+++ smoothloadinglib/src/main/java/com/prasanjit/smoothloadinglib/SmoothLoadingView.java
@@ -1,44 +1,73 @@
 package com.prasanjit.smoothloadinglib;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Toast;
 
-public class SmoothLoadingView extends AppCompatTextView {
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SmoothLoadingView extends AppCompatTextView implements View.OnClickListener{
 
     String TAG = getClass().getName();
-    private Paint paint;
+    private Paint backgroundPaint, paintText, linePaint;
     // private int size = 640;
     String labelPaint = "Some text";
-    Paint paintText;
+    String labelPaintLoad = "Loading...";
+    int cornerRadius;
+    int canvasWidth = 96;
+    int canvasHeight = 48;
+    int actualWidth;
+    int actualHeight;
+    boolean isAnimated = false;
+    int repeatCounter = 0;
+    Context context;
 
     public SmoothLoadingView(Context context) {
         super(context);
+        this.context = context;
         init(null);
     }
 
-    public SmoothLoadingView(Context context, AttributeSet attrs){
+    public SmoothLoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         init(attrs);
     }
 
-    private void init(AttributeSet attrs){
-        paint = new Paint();
-        paintText = new Paint();
-        paintText.setColor(Color.BLACK);
-        paintText.setTextSize(72);
+    private void init(AttributeSet attrs) {
+        backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintText.setColor(Color.WHITE);
+        // text size of 16 sp
+        paintText.setTextSize(Math.round(16f * getResources().getDisplayMetrics().scaledDensity));
+
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(ContextCompat.getColor(context, android.R.color.holo_red_light));
+        linePaint.setStrokeWidth(1f);
+
+        setOnClickListener(this);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(getResources().getColor(android.R.color.holo_green_light));
+        backgroundPaint.setColor(getResources().getColor(android.R.color.holo_green_light));
         // paint.setStyle(Paint.Style.FILL);
 
         // float radius = size / 2f;
@@ -46,17 +75,39 @@ public class SmoothLoadingView extends AppCompatTextView {
         // canvas.drawColor(getResources().getColor(android.R.color.holo_green_light));
 
         int offset = 32;
-        int canvasWidth = canvas.getWidth();
-        int canvasHeight = canvas.getHeight();
-        Log.i(TAG, canvasWidth + " * " + canvasHeight);
+
+        actualWidth = canvas.getWidth();
+        actualHeight = canvas.getHeight();
+
+        // horizontal center
+        final float centerX = actualWidth * 0.5f;
+
+        Log.i(TAG, "canvas width " + actualWidth + " * " + "canvas height " + actualHeight);
+
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
         RectF rectF = new RectF(offset, offset, canvasWidth - offset, canvasHeight - offset);
-        int cornerRadius = 25;
-        canvas.drawRoundRect(rectF, cornerRadius, cornerRadius, paint);
+
+        cornerRadius = Math.round(2f * getResources().getDisplayMetrics().density);
+
+        Log.i(TAG, "corner radius " + cornerRadius);
+        canvas.drawRoundRect(rectF, cornerRadius, cornerRadius, backgroundPaint);
+
+        // Draw baseline.
+        final float baselineY = Math.round(canvasHeight * 0.6f);
+        // canvas.drawLine(0, baselineY, canvasWidth, baselineY, linePaint);
 
         // canvas.drawText(labelPaint, rectF.left + offset, rectF.top - offset, paintText);
         // canvas.drawRect(0 , 0, 480 , 196, paint);
         // drawLabel(canvas);
-        drawText(canvas, paintText, labelPaint);
+        if(isAnimated) {
+            // drawText(canvas, paintText, labelPaintLoad);
+            drawTextAnother(canvas, paintText, labelPaintLoad, centerX, baselineY);
+        }else {
+            // drawText(canvas, paintText, labelPaint);
+            drawTextAnother(canvas, paintText, labelPaint, centerX, baselineY);
+        }
     }
 
     @Override
@@ -101,8 +152,7 @@ public class SmoothLoadingView extends AppCompatTextView {
         canvas.drawText(labelPaint, x, y, paintText);
     }
 
-    private void drawText(Canvas canvas, Paint paint, String text){
-
+    private void drawText(Canvas canvas, Paint paint, String text) {
         Rect r = new Rect();
         canvas.getClipBounds(r);
         int cHeight = r.height();
@@ -114,4 +164,135 @@ public class SmoothLoadingView extends AppCompatTextView {
         canvas.drawText(text, x, y, paint);
     }
 
+    private void drawTextAnother(Canvas canvas, Paint paint, String text, float centerX, float baselineY){
+        // Measure the width of text to display.
+        final float textWidth = paint.measureText(text);
+        // Figure out an x-coordinate that will center the text in the canvas.
+        final float textX = Math.round(centerX - textWidth * 0.5f);
+
+        // Draw.
+        canvas.drawText(text, textX, baselineY, paint);
+    }
+
+    private void setAnimate(){
+        repeatCounter++;
+        ValueAnimator widthAnimator = ValueAnimator.ofInt(1, actualWidth/8);
+        widthAnimator.setDuration(500);
+        widthAnimator.setInterpolator(new DecelerateInterpolator());
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // canvasWidth = (int) animation.getAnimatedValue();
+                // vasHeight = (int) animation.getAnimatedValue();
+                SmoothLoadingView.this.setTranslationX((int) animation.getAnimatedValue());
+                Log.i(TAG, "Value " + (int) animation.getAnimatedValue());
+                if(actualWidth/8 == (int) animation.getAnimatedValue()){
+                    if(repeatCounter < 50) {
+                        setReverseAnimate();
+                    }
+                }
+                // SmoothLoadingView.this.setTranslationY((float) animation.getAnimatedValue());
+                SmoothLoadingView.this.invalidate();
+            }
+        });
+        // widthAnimator.setRepeatCount(5);
+        widthAnimator.start();
+        invalidate();
+    }
+
+    private void setReverseAnimate(){
+        repeatCounter++;
+        ValueAnimator widthAnimator = ValueAnimator.ofInt(actualWidth/8, 1);
+        widthAnimator.setDuration(500);
+        widthAnimator.setInterpolator(new DecelerateInterpolator());
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // canvasWidth = (int) animation.getAnimatedValue();
+                // vasHeight = (int) animation.getAnimatedValue();
+                SmoothLoadingView.this.setTranslationX((int) animation.getAnimatedValue());
+                Log.i(TAG, "Value " + (int) animation.getAnimatedValue());
+                if(1 == (int) animation.getAnimatedValue()){
+                    if(repeatCounter < 50) {
+                        setAnimate();
+                    }else {
+                        setAnimateFast();
+                    }
+                }
+                // SmoothLoadingView.this.setTranslationY((float) animation.getAnimatedValue());
+                SmoothLoadingView.this.invalidate();
+            }
+        });
+        // widthAnimator.setRepeatCount(5);
+        widthAnimator.start();
+        invalidate();
+    }
+
+    private void setAnimateFast(){
+        repeatCounter++;
+        ValueAnimator widthAnimator = ValueAnimator.ofInt(1, actualWidth);
+        widthAnimator.setDuration(200);
+        widthAnimator.setInterpolator(new DecelerateInterpolator());
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                SmoothLoadingView.this.setTranslationX((int) animation.getAnimatedValue());
+                SmoothLoadingView.this.invalidate();
+            }
+        });
+        // widthAnimator.setRepeatCount(5);
+        widthAnimator.start();
+        invalidate();
+    }
+
+    private void fadeIn(){
+        ValueAnimator widthAnimator = ValueAnimator.ofFloat(0f, 1f);
+        widthAnimator.setDuration(10000);
+        widthAnimator.setInterpolator(new DecelerateInterpolator());
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fadeValue = (float) animation.getAnimatedValue();
+                SmoothLoadingView.this.setAlpha(fadeValue);
+                if(fadeValue == 1f){
+                    fadeOut();
+                }
+                SmoothLoadingView.this.invalidate();
+            }
+        });
+        // widthAnimator.setRepeatCount(5);
+        widthAnimator.start();
+        invalidate();
+    }
+
+    private void fadeOut(){
+        ValueAnimator widthAnimator = ValueAnimator.ofFloat(0f, 1f);
+        widthAnimator.setDuration(5000);
+        widthAnimator.setInterpolator(new DecelerateInterpolator());
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fadeValue = (float) animation.getAnimatedValue();
+                SmoothLoadingView.this.setAlpha(fadeValue);
+                if(fadeValue == 0.25f){
+                    fadeIn();
+                }
+                SmoothLoadingView.this.invalidate();
+            }
+        });
+        // widthAnimator.setRepeatCount(5);
+        widthAnimator.start();
+        invalidate();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(isAnimated == false) {
+            isAnimated = true;
+            setAnimate();
+            fadeIn();
+        }else {
+            // do nothing
+        }
+    }
 }
